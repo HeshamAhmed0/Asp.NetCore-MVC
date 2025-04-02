63f5061c7fbcd521cf2bd7ddf3e25b6a7b566ed2
Hesham Mathana <heshammathana0@gmail.com>
2025-04-02 14:21:51 +0200

    This For My Study For MVC

---
+++ Session01/What Is Asp.Net/What Is Asp.Net/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace What_Is_Asp.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+           ** to use any Techonology yoou must use [ SDK ] For It
+           
+           ** Http && Https => The deferent between them is [Secure]
+           ** URL => By Location 
+           **URN => By Name 
+           ** URI => By Name And Location 
+           **FTP => Transfer Files Between Client And Server ==> Send And Receive
+           **TCP To Sure If Data Send True or not
+           *                  Server 
+           *   1 : Physical Server => This is hardware belong to me 
+           *   2 : Virtual Server => This is Software Belong to me 
+             
+             */
+        }
+    }
+}
