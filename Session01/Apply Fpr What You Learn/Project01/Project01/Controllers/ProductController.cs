41756d709e5f39c89754506632e0cf840b007ee2
Hesham Mathana <heshammathana0@gmail.com>
2025-04-03 08:15:46 +0200

    Apply For My FirstProject By MVC

---
+++ Session01/Apply Fpr What You Learn/Project01/Project01/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Project01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ContentResult ShowContest()
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "Hello Hesham";
+            return result;
+        }
+        public ViewResult viewResult()
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "View1";
+            return result;
+        }
+
+        public JsonResult ShowwJson()
+        {
+            JsonResult jsonResult = new JsonResult(new {Id =20 , Name = "hassan"});
+            return jsonResult;  
+            
+        }
+        public IStatusCodeActionResult ShowRE(int id)
+        {
+            if(id%2== 0)
+            {
+                ViewResult result = new ViewResult();
+                result.ViewName = "View1";
+                return result;
+            }
+            else
+            {
+                JsonResult jsonResult = new JsonResult(new { Id = 20, Name = "hassan" });
+                return jsonResult;
+            }
+        }
+    }
+
+}
