0c72b6c7c45dcd82ce0318216a0862be370d1292
Hesham Mathana <heshammathana0@gmail.com>
2025-04-04 02:17:44 +0200

    Apply

--- Session01/Apply Fpr What You Learn/Project02/project02/project02/Program.cs
+++ Session01/Apply Fpr What You Learn/Project02/project02/project02/Program.cs
@@ -5,10 +5,29 @@ namespace project02
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            #region Configure Servies
+             builder.Services.AddControllersWithViews();
+            #endregion
+                   
             var app = builder.Build();
 
+
+            #region SEGMENTS
+
+            #region Static SEGMENTS
             app.MapGet("/", () => "Hello World!");
 
+            #endregion
+            #region Variable SEGMENTS
+            app.MapGet("/{name}", async context =>
+            {
+                var Name = context.GetRouteValue("name");
+                await context.Response.WriteAsync($"Hello {Name}");
+            });
+
+            #endregion
+
+            #endregion
             app.Run();
         }
     }
