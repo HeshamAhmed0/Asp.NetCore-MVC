dc4efeb6d3f604e6698efbd974a500eb32d8cde7
Hesham Mathana <heshammathana0@gmail.com>
2025-04-04 23:32:16 +0200

    Apply For Class Controller And How Use it

---
+++ Session01/Apply Fpr What You Learn/Project02/project02/project02/Contllorer/ViewContllorer.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+namespace project02.Contllorer
+{
+    public class ViewContllorer : Controller
+    {
+        public string GetView()
+        {
+            return "Hello In View";
+        }
+    }
+}
