c48b915b67dc43374394ebbc7673bc0aa980c85a
Hesham Mathana <heshammathana0@gmail.com>
2025-04-04 18:55:25 +0200

    Apply

--- Session01/Apply Fpr What You Learn/Project01/Project01/Controllers/ProductController.cs
+++ Session01/Apply Fpr What You Learn/Project01/Project01/Controllers/ProductController.cs
@@ -23,11 +23,18 @@ namespace Project01.Controllers
             return result;
         }
 
+        //public JsonResult ShowwJson()
+        //{
+        //    JsonResult jsonResult = new JsonResult(new {Id =20 , Name = "hassan"});
+        //    return jsonResult;  
+
+        //}
+
         public JsonResult ShowwJson()
         {
-            JsonResult jsonResult = new JsonResult(new {Id =20 , Name = "hassan"});
-            return jsonResult;  
-            
+            JsonResult jsonResult = new JsonResult(new { Id = 20, Name = "hassan" });
+            return jsonResult;
+
         }
         public IStatusCodeActionResult ShowRE(int id)
         {
