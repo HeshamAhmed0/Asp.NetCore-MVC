200dda08d99aa04f9ffd66838013f769eff7ca16
Hesham Mathana <heshammathana0@gmail.com>
2025-04-13 07:45:29 +0200

    Apply For Session02

---
+++ Session02/Session02/Controllers/ApplicationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Session02.Controllers
+{
+    public class ApplicationController :Controller
+    {
+        public string GetAction(int? Id ,string Name)
+        {
+            return $"Id : {Id} , Name : {Name}";
+        }
+    }
+}
