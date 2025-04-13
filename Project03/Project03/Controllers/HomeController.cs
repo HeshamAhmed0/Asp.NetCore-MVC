0681fa097e5a10606ae9af4cf20bad4c1aa4d2b6
Hesham Mathana <heshammathana0@gmail.com>
2025-04-13 07:44:57 +0200

    Apply For Session02

---
+++ Project03/Project03/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string ReturnResul(int id)
+        {
+            if (id == 0)
+                return $"Id {id}";
+            else
+                return $"Id {id}";
+        }
+       
+    }
+}
