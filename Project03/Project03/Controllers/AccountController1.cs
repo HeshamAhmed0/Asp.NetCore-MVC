e50e31a9ffe4082a0222319a1a77f25b83084495
Hesham Mathana <heshammathana0@gmail.com>
2025-04-15 22:47:07 +0200

    Apply For Layout

---
+++ Project03/Project03/Controllers/AccountController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project03.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
