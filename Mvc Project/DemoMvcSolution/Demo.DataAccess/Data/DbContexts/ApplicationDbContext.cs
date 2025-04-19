4414d8e3d51079af4de57df2da1516449158e273
Hesham Mathana <heshammathana0@gmail.com>
2025-04-19 23:18:07 +0200

    Apply For What You learn in Session03

--- Mvc Project/DemoMvcSolution/Demo.DataAccess/Data/DbContexts/ApplicationDbContext.cs
+++ Mvc Project/DemoMvcSolution/Demo.DataAccess/Data/DbContexts/ApplicationDbContext.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System.Reflection;
+using Demo.DataAccess.Data.Configrations;
+using Demo.DataAccess.Repositories;
 
 namespace Demo.DataAccess.Data.DbContexts
 {
-    internal class ApplicationDbContext
-    {
+    public class ApplicationDbContext:DbContext
+    { 
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Connection String");
+        //}
+
+        // this is Use For Apply Configrations 
+
+        public DbSet<Department> Departments { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // in this case will configur automatic
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+            // this way is best
+           modelBuilder.ApplyConfigurationsFromAssembly(typeof(DepartmentConfigration).Assembly);
+        }
+
+
     }
 }
