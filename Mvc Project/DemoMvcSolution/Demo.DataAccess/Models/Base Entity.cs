c08e00d0eccacadd5a5cab76cfe0fa4bcfa4ac68
Hesham Mathana <heshammathana0@gmail.com>
2025-04-19 23:18:18 +0200

    Apply For What You learn in Session03

--- Mvc Project/DemoMvcSolution/Demo.DataAccess/Models/Base Entity.cs
+++ Mvc Project/DemoMvcSolution/Demo.DataAccess/Models/Base Entity.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Demo.DataAccess.Models
 {
-    internal class Base_Entity
+    public class Base_Entity
     {
+        public int Id { get; set; } //Pk
+        public int CreatedBy { get; set; } //User Id
+        public DateTime CreatedOn { get; set; } //Current  Time
+        public int LastModefiedBy { get; set; } //User Id
+        public DateTime LastModifiedOn { get; set; } //Time of update
+        public bool IsDeleted { get; set; } //flag for deel with database
+
     }
 }
