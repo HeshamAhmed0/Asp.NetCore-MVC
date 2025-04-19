48f6fa348a796bb1c2fb93868bcb4d045d43bd68
Hesham Mathana <heshammathana0@gmail.com>
2025-04-19 23:17:57 +0200

    Apply For What You learn in Session03

--- Mvc Project/DemoMvcSolution/Demo.DataAccess/Data/Configrations/DepartmentConfigration.cs
+++ Mvc Project/DemoMvcSolution/Demo.DataAccess/Data/Configrations/DepartmentConfigration.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Demo.DataAccess.Data.Configrations
 {
-    internal class ApplicationConfigration
+    internal class DepartmentConfigration : IEntityTypeConfiguration<Department>
     {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10,10);
+            builder.Property(D => D.Name).HasColumnType("Varchar(20)");
+            builder.Property(D => D.Code).HasColumnType("Varchar(20)");
+            builder.Property(D => D.CreatedOn).HasDefaultValueSql("GETDATE()");
+            builder.Property(D => D.CreatedOn).HasComputedColumnSql("GETDATE()");
+        }
+        
     }
 }
--- Mvc Project/DemoMvcSolution/Demo.DataAccess/GlobalUsing.cs
+++ Mvc Project/DemoMvcSolution/Demo.DataAccess/GlobalUsing.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Demo.DataAccess
-{
-    internal class GlobalUsing
-    {
-    }
-}
+﻿global using Demo.DataAccess.Models;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+ 
