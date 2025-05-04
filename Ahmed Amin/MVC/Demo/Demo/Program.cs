d1684d0265117552bc6ca81a09f68980e7fb9774
Hesham <heshammathana0@gmail.com>
2025-05-04 23:53:40 +0300

    Apply For MVC

--- Ahmed Amin/MVC/Demo/Demo/Program.cs
+++ Ahmed Amin/MVC/Demo/Demo/Program.cs
@@ -9,10 +9,10 @@ namespace Demo
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddControllersWithViews();
             var app = builder.Build();
-
+            app.UseStaticFiles();
             //app.MapGet("/", () => "Hello World!");
             app.MapControllerRoute(name : "Default",
-                pattern:"/{Controller}/{Action}/{Id?}"
+                pattern:"/{Controller=Home}/{Action=Index}/{Id?}"
                 );
 
             app.Run();
